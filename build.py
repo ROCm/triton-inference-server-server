97b8dad3aa80f2fafc493d9e2831cda2cbe5c841
linsun12 <linsun12@amd.com>
2026-03-20 20:45:19 -0400

    onnxruntime target compile batch fix merged to rocm7.2_internal_testing

--- build.py
+++ build.py
@@ -3200,7 +3200,7 @@ if __name__ == "__main__":
         "--ort-branch",
         required=False,
         type=str,
-        default="remove_target_batch_compile_residule",
+        default="rocm7.2_internal_testing",
         help="ONNX Runtime (ROCm) git branch when building from source. Used by onnxruntime backend.",
     )
     parser.add_argument(
